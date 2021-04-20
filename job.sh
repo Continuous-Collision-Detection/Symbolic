7d5aa983fe71056cb15c5b3637cb1017195e679a
teseoch <teseo@bluewin.ch>
2021-04-20 09:28:04 -0700

    Create job.sh

---
+++ job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=96:00:00
+#SBATCH --mem=4GB
+
+# Load modules
+module purge
+module load mathematica/12.1.1
+module load python/intel/3.8.6
+
+# Run job
+cd <path>
+python check_roots.py $1 $2 $3 $4 <mathematica-kernel> out <root-folder>
